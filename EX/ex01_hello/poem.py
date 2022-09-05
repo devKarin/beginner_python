c5051687e2b206a7b5064bc4d98ffece9860e93f
Karin Kikas <karin.kikas@gmail.com>
2022-09-06 00:01:55 +0300

    Add 2. Poem.

---
+++ EX/ex01_hello/poem.py
@@ -0,0 +1,23 @@
+"""EX01 Poem."""
+
+"""
+2. Poem
+Example output:
+
+Roses are red,
+violets are blue,
+I love to code
+And so will you!
+
+"""
+
+
+rose_color = "red"
+flowers_name = "violets"
+favourite_activity = "code"
+
+rose_color = input(f"Roses are {rose_color}. What color suits instead of '{rose_color}'? ")
+flowers_name = input(f"{flowers_name} are blue. What flowers suit instead of '{flowers_name}'? ")
+favourite_activity = input(f"I love to {favourite_activity}. What activity suits instead of '{favourite_activity}'? ")
+
+print(f"Roses are {rose_color},\n{flowers_name} are blue,\nI love to {favourite_activity}\nAnd so will you!")
