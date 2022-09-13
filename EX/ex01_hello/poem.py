8ec675647c6907e226c2e908e0e8c32f4f93dba3
Karin Kikas <karin.kikas@gmail.com>
2022-09-13 23:15:21 +0300

    Add comments.

--- EX/ex01_hello/poem.py
+++ EX/ex01_hello/poem.py
@@ -16,8 +16,12 @@ rose_color = "red"
 flowers_name = "violets"
 favourite_activity = "code"
 
+# Ask for a rose color.
 rose_color = input(f"Roses are {rose_color}. What color suits instead of '{rose_color}'? ")
+# Ask for flowers name in plural.
 flowers_name = input(f"{flowers_name} are blue. What flowers suit instead of '{flowers_name}'? ")
+# Ask for an activity.
 favourite_activity = input(f"I love to {favourite_activity}. What activity suits instead of '{favourite_activity}'? ")
 
+# Print the poem with user-entered values.
 print(f"Roses are {rose_color},\n{flowers_name} are blue,\nI love to {favourite_activity}\nAnd so will you!")
