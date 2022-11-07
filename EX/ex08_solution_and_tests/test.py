8cd9bbc9a36dd930fbec7a8d55ab6bc32bbaa900
Karin Kikas <karin.kikas@gmail.com>
2022-11-07 19:45:33 +0200

    Now test of tests still passes. Add two more asserts - what happens now.

--- EX/ex08_solution_and_tests/test.py
+++ EX/ex08_solution_and_tests/test.py
@@ -68,8 +68,8 @@ def test__students_study__evening_no_coffee_needed():
     # assert students_study(22, False) is True
     assert students_study(18, True) is True
     assert students_study(18, False) is True
-    # assert students_study(24, True) is True
-    # assert students_study(24, False) is True
+    assert students_study(24, True) is True
+    assert students_study(24, False) is True
 
 
 # def test__students_study__morning_coffee_true():
