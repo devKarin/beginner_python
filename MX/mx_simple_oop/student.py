4234ddaf8e18cd3d903ac4b3f434b50246f85626
Karin Kikas <karin.kikas@gmail.com>
2022-12-22 03:48:16 +0200

    Add initial solution.

---
+++ MX/mx_simple_oop/student.py
@@ -0,0 +1,28 @@
+"""
+MX - Simple OOP.
+
+This program creates class Student.
+
+"""
+
+
+class Student:
+    """
+    Student class.
+
+    Student has name and finished status, which is by default false.
+    """
+    def __init__(self, name):
+        """
+        Initialize the Student class.
+
+        Student has name and finished status, which is by default false.
+        """
+        self.name = name
+        self.finished = False
+
+
+if __name__ == '__main__':
+    student = Student("John")
+    print(student.name)  # John
+    print(student.finished)  # False
