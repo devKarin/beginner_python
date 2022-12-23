7f826e1ec166f7616509757ca6801eafa500d116
Karin Kikas <karin.kikas@gmail.com>
2022-12-23 10:37:31 +0200

    Add some empty functions.

---
+++ WAT/wat/wat.py
@@ -0,0 +1,76 @@
+"""
+WAT
+
+At this point it is unclear what this module does.
+
+Available classes:
+unknown
+
+Available functions:
+unknown
+
+"""
+
+
+def function_a():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_b():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_c():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_d():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_e():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_f():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_g():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
+
+
+def function_h():
+    """
+    Unknown function.
+    :return:
+    """
+    pass
