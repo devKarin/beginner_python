6b1d75cf71c1af2a78b544d211c6e4b387690793
Karin Kikas <karin.kikas@gmail.com>
2023-01-02 00:29:56 +0200

    Fix style and add drive_to_line function.

--- EX/ex14_robot/robot.py
+++ EX/ex14_robot/robot.py
@@ -1,14 +1,27 @@
+"""
+EX14 - Robot.
+
+This program creates a line following robot using FollowerBot package.
+
+Available functions:
+test_run(robot: FollowerBot); Moves the robot compared to its starting position.
+drive_to_line(robot: FollowerBot);
+
+"""
+
+
 from FollowerBot import FollowerBot
 
 
 def test_run(robot: FollowerBot):
     """
-    Make the robot move, doesnt matter how much, just as long as it has moved from the starting position.
+    Make the robot move.
+
+    Assignment requirements: It does not matter how much the robot moves,
+    just as long as it has moved from the starting position.
 
     :param FollowerBot robot: instance of the robot that you need to make move
     """
-    # robot = FollowerBot()
-
     robot.set_wheels_speed(10)
     robot.sleep(2)
     robot.set_wheels_speed(0)
@@ -17,13 +30,28 @@ def test_run(robot: FollowerBot):
 
 def drive_to_line(robot: FollowerBot):
     """
-    Drive the robot until it meets a perpendicular black line, then drive forward 25cm.
+    Drive the robot until it meets a perpendicular black line, then drive forward 15-30cm.
 
     There are 100 pixels in a meter.
 
     :param FollowerBot robot: instance of the robot that you need to make move
     """
-    pass
+    # Move until two of the first sensors detect colour change to black.
+    while True:
+        # With these settings the robot should move about 3px during the cycle (100 * 0.03) which is
+        # less than the line width (5px).
+        robot.set_wheels_speed(100)
+        robot.sleep(0.03)
+
+        # Line sensor value 0 represents black and 1024 represents white.
+        if robot.get_left_line_sensor() == 0 and robot.get_right_line_sensor() == 0:
+            # Move the robot about 20px.
+            robot.set_wheels_speed(100)
+            robot.sleep(0.2)
+            # Then stop and break the cycle.
+            robot.set_wheels_speed(0)
+            robot.done()
+            break
 
 
 def follow_the_line(robot: FollowerBot):
@@ -48,4 +76,5 @@ def the_true_follower(robot: FollowerBot):
 
 if __name__ == "__main__":
     robot = FollowerBot()
-    test_run(robot)
+    # test_run(robot)
+    drive_to_line(robot)
